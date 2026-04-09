5f56965116de76a76fb4ece9845e48e2336d5b99
ÂReshma11K <reshmakarkal11@gmail.com>
2026-04-09 15:23:14 +0200

    fix cors for deployed frontend

--- backend/main.py
+++ backend/main.py
@@ -31,7 +31,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "http://127.0.0.1:5173",
+        "https://budget-expense-tracker-pi.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
